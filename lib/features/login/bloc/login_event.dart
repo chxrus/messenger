5e42c4dd4f136874b39715696c3bd2df13212a18
lukovkin-a-volsu <prib-222_676138@volsu.ru>
2024-07-27 22:25:12 +0300

    feat: add sign in with email and password

---
+++ lib/features/login/bloc/login_event.dart
@@ -0,0 +1,26 @@
+part of 'login_bloc.dart';
+
+sealed class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LoginInitialEvent extends LoginEvent {
+  @override
+  List<Object> get props => [];
+}
+
+final class LoginSubmitEvent extends LoginEvent {
+  const LoginSubmitEvent({
+    required this.email,
+    required this.password,
+  });
+
+  final String email;
+  final String password;
+
+  @override
+  List<Object> get props => [email, password];
+}
