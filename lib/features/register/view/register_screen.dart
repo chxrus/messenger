7a5247b958eb14842cb0ecec0380bbe21cff9631
lukovkin-a-volsu <prib-222_676138@volsu.ru>
2024-07-27 22:14:50 +0300

    feat: add register handling

--- lib/features/register/view/register_screen.dart
+++ lib/features/register/view/register_screen.dart
@@ -1,13 +1,44 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:messenger/features/register/bloc/register_bloc.dart';
 import 'package:messenger/generated/l10n.dart';
+import 'package:messenger/repositories/auth/auth.dart';
 import 'package:messenger/router/router.dart';
 import 'package:messenger/ui/ui.dart';
 
 @RoutePage()
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final _registerBloc = RegisterBloc(GetIt.I<AbstractAuthService>());
+
+  void register() {
+    if (_passwordController.text != _confirmPasswordController.text) {}
+    _registerBloc.add(
+      RegisterSubmitEvent(
+        email: _emailController.text,
+        password: _passwordController.text,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,44 +47,75 @@ class RegisterScreen extends StatelessWidget {
       onTap: FocusScope.of(context).unfocus,
       child: Scaffold(
         backgroundColor: theme.scaffoldBackgroundColor,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.person,
-                size: 36,
+        body: BlocListener<RegisterBloc, RegisterState>(
+          bloc: _registerBloc,
+          listener: (context, state) {
+            if (state is RegisterSubmitSuccessState) {
+              GetIt.I<AppRouter>().pushNamed('/messages');
+            }
+
+            if (state is RegisterSubmitFailureState) {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text(
+                      state.message,
+                      style: theme.textTheme.labelMedium,
+                    ),
+                  );
+                },
+              );
+            }
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 128),
+                    const Icon(
+                      Icons.person,
+                      size: 64,
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      S.of(context).registerTitle,
+                      style: theme.textTheme.titleMedium,
+                    ),
+                    const SizedBox(height: 32),
+                    CustomTextField(
+                      label: S.of(context).email,
+                      controller: _emailController,
+                    ),
+                    const SizedBox(height: 16),
+                    CustomTextField(
+                      label: S.of(context).newPassword,
+                      isObscure: true,
+                      controller: _passwordController,
+                    ),
+                    const SizedBox(height: 16),
+                    CustomTextField(
+                      label: S.of(context).confirmPassword,
+                      isObscure: true,
+                      controller: _confirmPasswordController,
+                    ),
+                    const SizedBox(height: 32),
+                    CustomButton(
+                      onPressed: register,
+                      label: S.of(context).registerButton,
+                    ),
+                    const SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () => GetIt.I<AppRouter>().pushNamed('/'),
+                      child: Text(S.of(context).alreadyRegistered),
+                    )
+                  ],
+                ),
               ),
-              const SizedBox(height: 12),
-              Text(
-                S.of(context).registerTitle,
-                style: theme.textTheme.titleMedium,
-              ),
-              const SizedBox(height: 36),
-              CustomTextField(
-                label: S.of(context).email,
-              ),
-              const SizedBox(height: 12),
-              CustomTextField(
-                label: S.of(context).newPassword,
-                isObscure: true,
-              ),
-              const SizedBox(height: 12),
-              CustomTextField(
-                label: S.of(context).confirmPassword,
-                isObscure: true,
-              ),
-              const SizedBox(height: 36),
-              CustomButton(
-                onPressed: () {},
-                label: S.of(context).registerButton,
-              ),
-              const SizedBox(height: 12),
-              TextButton(
-                onPressed: () => GetIt.I<AppRouter>().pushNamed('/'),
-                child: Text(S.of(context).alreadyRegistered),
-              )
-            ],
+            ),
           ),
         ),
       ),
