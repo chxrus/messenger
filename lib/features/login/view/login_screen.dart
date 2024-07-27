77c6371e1e82daa93a0b4b6c9b34e37cf2f65a8d
lukovkin-a-volsu <prib-222_676138@volsu.ru>
2024-07-27 22:24:19 +0300

    feat: add login handling

--- lib/features/login/view/login_screen.dart
+++ lib/features/login/view/login_screen.dart
@@ -1,14 +1,42 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:messenger/features/login/bloc/login_bloc.dart';
 import 'package:messenger/generated/l10n.dart';
+import 'package:messenger/repositories/auth/auth.dart';
 import 'package:messenger/router/router.dart';
-
-import '../../../ui/ui.dart';
+import 'package:messenger/ui/ui.dart';
 
 @RoutePage()
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _loginBloc = LoginBloc(GetIt.I<AbstractAuthService>())
+    ..add(LoginInitialEvent());
+
+  void login() {
+    _loginBloc.add(
+      LoginSubmitEvent(
+        email: _emailController.text,
+        password: _passwordController.text,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,38 +45,68 @@ class LoginScreen extends StatelessWidget {
       onTap: FocusScope.of(context).unfocus,
       child: Scaffold(
         backgroundColor: theme.scaffoldBackgroundColor,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.login,
-                size: 36,
+        body: BlocListener<LoginBloc, LoginState>(
+          bloc: _loginBloc,
+          listener: (context, state) {
+            if (state is LoginSubmitSuccessState) {
+              GetIt.I<AppRouter>().pushNamed('/messages');
+            }
+
+            if (state is LoginSubmitFailureState) {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text(
+                      state.message,
+                      style: theme.textTheme.labelMedium,
+                    ),
+                  );
+                },
+              );
+            }
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 128),
+                  const Icon(
+                    Icons.login,
+                    size: 64,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    S.of(context).loginTitle,
+                    style: theme.textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 32),
+                  CustomTextField(
+                    label: S.of(context).email,
+                    controller: _emailController,
+                  ),
+                  const SizedBox(height: 16),
+                  CustomTextField(
+                    label: S.of(context).password,
+                    controller: _passwordController,
+                    isObscure: true,
+                  ),
+                  const SizedBox(height: 32),
+                  CustomButton(
+                    onPressed: login,
+                    label: S.of(context).loginButton,
+                  ),
+                  const SizedBox(height: 16),
+                  TextButton(
+                    onPressed: () =>
+                        GetIt.I<AppRouter>().pushNamed('/register'),
+                    child: Text(S.of(context).notRegistered),
+                  )
+                ],
               ),
-              const SizedBox(height: 12),
-              Text(
-                S.of(context).loginTitle,
-                style: theme.textTheme.titleMedium,
-              ),
-              const SizedBox(height: 36),
-              CustomTextField(
-                label: S.of(context).email,
-              ),
-              const SizedBox(height: 12),
-              CustomTextField(
-                label: S.of(context).password,
-              ),
-              const SizedBox(height: 36),
-              CustomButton(
-                onPressed: () {},
-                label: S.of(context).loginButton,
-              ),
-              const SizedBox(height: 12),
-              TextButton(
-                onPressed: () => GetIt.I<AppRouter>().pushNamed('/register'),
-                child: Text(S.of(context).notRegistered),
-              )
-            ],
+            ),
           ),
         ),
       ),
