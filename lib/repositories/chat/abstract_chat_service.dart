5800fa103fb41d87ada0d602bf2bff6e9ccef867
chxrus <lukovkin.ark@gmail.com>
2024-08-16 11:02:06 +0300

    chore: rename

--- lib/repositories/chat/abstract_chat_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-abstract interface class AbstractChatService {
-  Stream<List<Map<String, dynamic>>> getUsersStream();
-  Future<void> sendMessage(String receiverID, String message);
-  Stream<QuerySnapshot> getMessagesStream(String userID, String otherUserID);
-}
