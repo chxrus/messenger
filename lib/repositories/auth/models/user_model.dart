77931019d46eb6fb14e6c63170cb005170b0ca2a
chxrus <lukovkin.ark@gmail.com>
2024-08-16 11:14:28 +0300

    feat: add name, photo and change to data class

---
+++ lib/repositories/auth/models/user_model.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable {
+  final String id;
+  final String? email;
+  final String? name;
+  final String? photoURL;
+
+  const UserModel({
+    required this.id,
+    this.email,
+    this.name,
+    this.photoURL,
+  });
+
+  static const UserModel empty = UserModel(id: '');
+
+  bool get isEmpty => this == UserModel.empty;
+
+  bool get isNotEmpty => this != UserModel.empty;
+
+  @override
+  List<Object> get props => [id];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'email': email,
+      'name': name,
+      'photoURL': photoURL,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['uid'] as String,
+      email: map['email'] != null ? map['email'] as String : null,
+      name: map['name'] != null ? map['name'] as String : null,
+      photoURL: map['photoURL'] != null ? map['photoURL'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) =>
+      UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  UserModel copyWith({
+    String? id,
+    String? email,
+    String? name,
+    String? photoURL,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      photoURL: photoURL ?? this.photoURL,
+    );
+  }
+}
