6c505ea3b6ac1821eb114940e1223e1e71d9e8e1
chxrus <lukovkin.ark@gmail.com>
2024-08-16 11:39:21 +0300

    refactor: add line between return and fields in build

--- lib/ui/widgets/custom_sliver_app_bar.dart
+++ lib/ui/widgets/custom_sliver_app_bar.dart
@@ -11,6 +11,7 @@ class CustomSliverAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appBarTheme = Theme.of(context).appBarTheme;
+
     return SliverAppBar(
       iconTheme: appBarTheme.iconTheme,
       actionsIconTheme: appBarTheme.actionsIconTheme,
