dd2983c7bddd7d0cf3930ce7f77eb1febf6fbdd0
chxrus <lukovkin.ark@gmail.com>
2024-08-16 10:45:31 +0300

    refactor: organize imports

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:messenger/app.dart';
 import 'package:messenger/firebase_options.dart';
 import 'package:messenger/router/router.dart';
 import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
+
+import 'app/app.dart';
 
 void main() async {
   runZonedGuarded(() async {
