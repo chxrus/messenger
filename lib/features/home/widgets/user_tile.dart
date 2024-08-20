6197e238812b61dba21d3ed703cc26c8e2e632ca
chxrus <lukovkin.ark@gmail.com>
2024-08-20 21:02:55 +0300

    feat: change sizes, add name text

--- lib/features/home/widgets/user_tile.dart
+++ lib/features/home/widgets/user_tile.dart
@@ -1,40 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get_it/get_it.dart';
-import 'package:messenger/repositories/chat/models/user_model.dart';
+import 'package:messenger/generated/l10n.dart';
+import 'package:messenger/repositories/auth/models/user_model.dart';
 import 'package:messenger/router/router.dart';
 
 class UserTile extends StatelessWidget {
   const UserTile({
     super.key,
-    required this.userModel,
+    required this.anotherUser,
   });
 
-  final UserModel userModel;
+  final UserModel anotherUser;
 
   @override
   Widget build(BuildContext context) {
+    final appRouter = GetIt.I<AppRouter>();
     final theme = Theme.of(context);
+
     return InkWell(
       highlightColor: theme.colorScheme.surfaceContainerLow,
       splashColor: Colors.transparent,
-      onTap: () => GetIt.I<AppRouter>().push(ChatRoute(userModel: userModel)),
+      onTap: () => appRouter.push(ChatRoute(user: anotherUser)),
       child: Container(
-        height: 75,
+        height: 85,
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Icon(
-              Icons.person,
-              size: 36,
-            ),
+            anotherUser.photoURL != null
+                ? SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.network(
+                        anotherUser.photoURL!,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+                : SvgPicture.asset(
+                    height: 50,
+                    'assets/svg/person.svg',
+                    colorFilter: ColorFilter.mode(
+                        theme.colorScheme.primary, BlendMode.srcIn),
+                  ),
             const SizedBox(width: 12),
             Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  userModel.email,
+                  anotherUser.name ?? S.of(context).loadingError,
                   style: theme.textTheme.labelSmall,
+                ),
+                Text(
+                  anotherUser.email ?? S.of(context).loadingError,
+                  style: theme.textTheme.bodyMedium,
                 ),
               ],
             )
