dc9f8ab76eb10379f70656ac5cf4a97ed86e3691
chxrus <lukovkin.ark@gmail.com>
2024-08-20 21:07:19 +0300

    feat: provide storage service

--- lib/app/app_initializer.dart
+++ lib/app/app_initializer.dart
@@ -5,6 +5,7 @@ import 'package:messenger/bloc/auth/auth_bloc.dart';
 import 'package:messenger/bloc/theme/theme_cubit.dart';
 import 'package:messenger/repositories/auth/auth.dart';
 import 'package:messenger/repositories/chat/chat.dart';
+import 'package:messenger/repositories/storage/storage.dart';
 
 class AppInitializer extends StatelessWidget {
   const AppInitializer({
@@ -21,7 +22,12 @@ class AppInitializer extends StatelessWidget {
         RepositoryProvider<IAuthService>(
           create: (context) => AuthService(),
         ),
-        RepositoryProvider<IChatService>(create: (context) => ChatService()),
+        RepositoryProvider<IChatService>(
+          create: (context) => ChatService(),
+        ),
+        RepositoryProvider<IStorageService>(
+          create: (context) => StorageService(),
+        ),
       ],
       child: MultiBlocProvider(providers: [
         BlocProvider(
